8b63d7bb9dd66011266573d18289a297bac761d5
ChandraKiran Bolla <bollachandrak@gmail.com>
2024-12-25 16:02:26 -0600

    Race Condition

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 public class Main {
     public static void main(String[] args) {
 
-
     }
 }
 
@@ -482,3 +481,41 @@ public class Main {
 //        obj2.start();
 //        obj1.show();
 //        obj2.show();
+
+// Threads Race Condition
+//class Counter{
+//    int count;
+//    public synchronized void increment(){
+//        count++;
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) throws InterruptedException {
+//
+//        Counter c = new Counter();
+//
+//        Runnable obj1 = () -> {
+//            for (int i = 0; i < 5; i++) {
+//                c.increment();
+//            }
+//        };
+//
+//        Runnable obj2 = () -> {
+//            for (int i = 0; i < 5; i++) {
+//                c.increment();
+//            }
+//        };
+//
+//        Thread t1 = new Thread(obj1);
+//        Thread t2 = new Thread(obj2);
+//
+//        t1.start();
+//        t2.start();
+//
+//        t1.join();
+//        t2.join();
+//
+//        System.out.println(c.count);
+//    }
+//}
